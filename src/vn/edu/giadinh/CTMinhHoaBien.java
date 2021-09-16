e4fd8772c7d7f055345411bc52018e1debc35212
PDTuanAnh <pdta1310@gmail.com>
2021-09-16 18:15:15 +0700

    initial commit

---
+++ src/vn/edu/giadinh/CTMinhHoaBien.java
@@ -0,0 +1,13 @@
+package vn.edu.giadinh;
+
+public class CTMinhHoaBien {
+    public static void main(String[] args) {
+        float diemLapTrinhHDT = 0.5F;
+
+        double diemKTLapTrinh;
+
+        diemKTLapTrinh = 10.0;
+
+        System.out.println("Điểm môn Kỹ thuật lập trinh "+diemKTLapTrinh);
+    }
+}
---
+++ src/vn/edu/giadinh/CTMinhHoaNhapXuat.java
@@ -0,0 +1,9 @@
+package vn.edu.giadinh;
+
+public class CTMinhHoaNhapXuat {
+    public static void main(String[] args) {
+        System.out.print("Gia Định ");
+        System.out.println("Đào tạo");
+        System.out.printf("%d nghề", 12);
+    }
+}
