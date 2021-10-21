fdfe03a360afae8524350963e20c80e3d13e413c
PDTuanAnh <pdta1310@gmail.com>
2021-10-21 08:20:35 +0700

    Lab6

--- src/com/anhpdt2008110004/Lab6/NhanXet.java
+++ src/com/anhpdt2008110004/Lab6/NhanXet.java
@@ -3,5 +3,6 @@ package com.anhpdt2008110004.Lab6;
 public class NhanXet {
     public static void main(String[] args) {
         System.out.println("Các chương trình chạy tốt, không lỗi.");
+        System.out.println("Code gần như là trùng nhau.");
     }
 }
